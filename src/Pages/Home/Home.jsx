443b437d2734695e6846f4986cf1b2e40ed4cad0
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-07-16 16:50:47 -0300

    style: Ajuste na responsividade da grid principal

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -11,7 +11,7 @@ function Home() {
   }, []);
   return (
     <main className="container mx-auto">
-      <div className="grid grid-cols-2 lg:grid-cols-3 gap-4">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         {products.map((product) => {
           return <Card key={product.id} {...product} />;
         })}
