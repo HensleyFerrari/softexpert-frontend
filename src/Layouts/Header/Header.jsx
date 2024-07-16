7443eec81a048a5e2fa141d46e148ac2f7387e42
HensleyFerrari <hensley_ferrari@hotmail.com>
2024-07-16 16:33:07 -0300

    style: icones adicionados no menu

--- src/Layouts/Header/Header.jsx
+++ src/Layouts/Header/Header.jsx
@@ -1,23 +1,34 @@
 import { Link } from "react-router-dom";
+import SoftLogo from "../../assets/softExpert-logo.png";
+import { IoCartOutline } from "react-icons/io5";
+import { CiShop } from "react-icons/ci";
+import { CiSquarePlus } from "react-icons/ci";
 
 function Header() {
   return (
     <nav className="flex justify-between py-6 px-8 bg-blue-800 text-white">
-      <h1>SoftExpert</h1>
-      <div className="flex gap-4">
-        <Link className="hover:text-gray-400" to="/">
-          Produtos
+      <Link to="/">
+        <img src={SoftLogo} alt="SoftExpert Logo" className="h-10" />
+      </Link>
+      <div className="flex gap-4 self-center font-semibold">
+        <Link className="hover:text-blue-200 flex gap-2" to="/">
+          <CiShop className="self-center" size={25} />
+          <span>Produtos</span>
         </Link>
-        <Link className="hover:text-gray-400" to="/shoppingCart">
-          Carrinho
+        <Link className="hover:text-blue-200 flex gap-2" to="/shoppingCart">
+          <IoCartOutline className="self-center" size={25} />
+          <span>Carrinho</span>
         </Link>
-        <Link className="hover:text-gray-400" to="/createType">
-          Cadastrar Tipo
+      </div>
+      <div className="flex gap-4 self-center font-semibold">
+        <Link className="hover:text-blue-200 flex gap-2" to="/createType">
+          <CiSquarePlus className="self-center" size={25} />
+          <span>Cadastrar Tipo</span>
         </Link>
-        <Link className="hover:text-gray-400" to="/createProduct">
-          Cadastrar Produto
+        <Link className="hover:text-blue-200 flex gap-2" to="/createProduct">
+          <CiSquarePlus className="self-center" size={25} />
+          <span>Cadastrar Produto</span>
         </Link>
-
       </div>
     </nav>
   );
